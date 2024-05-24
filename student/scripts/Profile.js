ab349d131d83c5a2daccc5781cb1cd0fbced5e4c
Mahmoud Mohammed Nael <mahmoudmnael@gmail.com>
2024-05-25 02:14:32 +0300

    wussup tany

--- student/scripts/Profile.js
+++ student/scripts/Profile.js
@@ -1,9 +1,21 @@
-let modback = JSON.parse(localStorage.getItem('user'));
+let modback = {};
+
+let req = new XMLHttpRequest();
+req.open('GET', 'http://localhost:8000/api/auth/profile/', false);
+req.withCredentials = true;
+req.onload = function () {
+	if (req.status == 200) {
+		modback = JSON.parse(req.responseText);
+	} else {
+		console.log('Error');
+	}
+};
+req.send();
 
 let contentEditor = document.querySelectorAll('.datalist li');
 
 console.log(modback);
-contentEditor[0].innerHTML = `<b>Full name :</b> ${modback.name}`
-contentEditor[1].innerHTML = `<b>Email :</b> ${modback.email}`
-contentEditor[2].innerHTML = `<b>Role :</b> ${modback.role}`
-contentEditor[3].innerHTML = `<b>ID :</b> ${modback.id}`
+contentEditor[0].innerHTML = `<b>Full name :</b> ${modback.full_name}`;
+contentEditor[1].innerHTML = `<b>Email :</b> ${modback.email}`;
+contentEditor[2].innerHTML = `<b>Role :</b> ${modback.role}`;
+contentEditor[3].innerHTML = `<b>ID :</b> ${modback.id}`;
--- teacher/scripts/view_people.js
+++ teacher/scripts/view_people.js
@@ -1,22 +1,54 @@
-let AdminListEditor = document.querySelector(".Admins_table");
-let TeacherListEditor = document.querySelector(".Teachers_table");
-let StudentListEditor = document.querySelector(".Students_table");
+let moddata = [];
+let classroomID = sessionStorage.getItem('classroomID');
 
-let modback = localStorage.getItem('RegisteredUsers')
-let moddata = JSON.parse(modback)
+let req = new XMLHttpRequest();
+/*
+    we will get the classroom id from session storage
+    we will then use the classroom id to get the list of teachers and students in the classroom
+ */
+req.open(
+	'GET',
+	`http://localhost:8000/api/classrooms/${classroomID}/people/`,
+	false
+);
+req.withCredentials = true;
+req.onload = function () {
+	if (req.status == 200) {
+		moddata = JSON.parse(req.responseText);
+	} else {
+		console.log('Error');
+	}
+};
+req.send();
 
-console.log(moddata)
+let TeacherListEditor = document.querySelector('.Teachers_table');
+let StudentListEditor = document.querySelector('.Students_table');
 
-moddata.forEach((item) => {
-    if(item.role==='admin'){
-        AdminListEditor.innerHTML+=`<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${item.name}</li>`
-    }
-    else if (item.role==='teacher'){
-        TeacherListEditor.innerHTML+=`<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${item.name}</li>`
-    }
-    else{
-        StudentListEditor.innerHTML+=`<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${item.name}</li>`
-    }
-    
+console.log(moddata);
+
+TeacherListEditor.innerHTML += `<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${moddata.teacher.full_name}</li>`;
+
+moddata.students.forEach((item) => {
+	StudentListEditor.innerHTML += `<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${item.full_name}</li>`;
 });
+let searchresult = document.getElementById('input-search');
+// i want to display the list of teachers and students in the classroom based on the search result
+// i will use the searchresult variable to get the search result
+// i will then use the moddata variable to get the list of teachers and students
+// i will then display the list of teachers and students based on the search result
 
+searchresult.addEventListener('keyup', function () {
+	let search = searchresult.value.toLowerCase();
+	let TeacherListEditor = document.querySelector('.Teachers_table');
+	let StudentListEditor = document.querySelector('.Students_table');
+	TeacherListEditor.innerHTML = '';
+	StudentListEditor.innerHTML = '';
+	moddata.students.forEach((item) => {
+		if (item.full_name.toLowerCase().includes(search)) {
+			StudentListEditor.innerHTML += `<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${item.full_name}</li>`;
+		}
+	});
+	if (moddata.teacher.full_name.toLowerCase().includes(search)) {
+		TeacherListEditor.innerHTML += `<li><img src="../assets/images/simple_profile_photo.png" alt="profile photo">${moddata.teacher.full_name}</li>`;
+	}
+});
